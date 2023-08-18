9a46ba9ec03ff3844a146d27542063914237af8f
Emily Valarezo <136937944+emilybibi04@users.noreply.github.com>
2023-08-17 19:28:37 -0500

    Interfaz de Inicio de sesion

--- src/main/java/com/pooespol/proyecto_2p_poo/App.java
+++ src/main/java/com/pooespol/proyecto_2p_poo/App.java
@@ -11,23 +11,15 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    
+    public void start(Stage s) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Inicio.fxml"));
+        Parent root = fxmlLoader.load();
+        scene = new Scene(root, 1240, 700);
+        s.setScene(scene);
+        s.show();
     }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
+    
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.pooespol.proyecto_2p_poo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.pooespol.proyecto_2p_poo to javafx.fxml;
     exports com.pooespol.proyecto_2p_poo;
